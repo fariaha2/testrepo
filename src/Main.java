3a2b30b5f3395c9b389523501743d93adfe3a9e1
BT_2N2_22 <hafkfgl@bths.edu>
2023-12-15 13:56:58 -0500

    COMMITTT4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!fdgjj");
+        System.out.println("Hello world!$%*&fdgjj");
         System.out.println("hihihi");
         System.out.println("skg");
         System.out.println("JDKFJAFHK");
